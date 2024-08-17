b1dd58a99943c489459015e4cd6a6919d3a5f191
sergey <mamban1997@gmail.com>
2024-08-17 14:13:28 +0500

    fix routes in kafka and expose swagger url

--- compose/kong-setup.sh
+++ compose/kong-setup.sh
@@ -13,12 +13,18 @@ curl -i -X POST http://kong:8001/services/ \
   --data name=telemetry \
   --data url=http://telemetry:8080
 
+curl -s -X POST http://kong:8001/services/device-management/routes \
+  --data "paths[]=/devices/" \
+  --data "strip_path=true"
+
+curl -s -X POST http://kong:8001/services/telemetry/routes \
+  --data "paths[]=/telemetry/" \
+  --data "strip_path=true"
 
 curl -i -X POST http://kong:8001/services/device-management/routes \
-  --data "paths[]=/api/devices"\
+  --data "paths[]=/api/devices/"\
   --data "strip_path=false"
 
 curl -i -X POST http://kong:8001/services/telemetry/routes \
-  --data "paths[]=/api/telemetry"\
+  --data "paths[]=/api/telemetry/"\
   --data "strip_path=false"
-
